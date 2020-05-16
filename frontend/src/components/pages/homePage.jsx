a1dd2514a4b26626e01a7e17b9fe491b4fe9f0c2
Ankan Poddar <friendyankan@gmail.com>
2020-05-16 13:10:48 +0530

    [FRONTEND] Remove unused imports from homepage and landing page

--- frontend/src/components/pages/homePage.jsx
+++ frontend/src/components/pages/homePage.jsx
@@ -1,7 +1,6 @@
 import React from "react";
 import { Typography } from "@material-ui/core";
 import { makeStyles } from "@material-ui/core/styles";
-import { GoogleLogoutButton } from "../widgets";
 
 const useStyles = makeStyles(() => {
   return {
--- frontend/src/components/pages/landingPage.jsx
+++ frontend/src/components/pages/landingPage.jsx
@@ -1,7 +1,6 @@
 import React from "react";
 import { Typography } from "@material-ui/core";
 import { makeStyles } from "@material-ui/core/styles";
-import { GoogleLoginButton } from "../widgets";
 
 const useStyles = makeStyles((theme) => {
   return {
